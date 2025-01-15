2f2bc942017e6109270d294ef649cb9f279a927e
thuvasooriya <thuvaragan28@gmail.com>
2025-01-15 14:41:26 +0530

    cleanup and update readme

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   description = "development environment for orb-slam3";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = {
     self,
@@ -16,7 +13,6 @@
       "aarch64-darwin"
     ];
 
-    # helper function to generate attrs for each system
     forAllSystems = f:
       nixpkgs.lib.genAttrs supportedSystems (system:
         f {
@@ -24,7 +20,6 @@
           inherit system;
         });
 
-    # platform-specific configurations
     platformConfig = {
       pkgs,
       system,
@@ -32,32 +27,31 @@
       isLinux = pkgs.stdenv.isLinux;
       isDarwin = pkgs.stdenv.isDarwin;
     in {
-      # platform-specific build inputs
       buildInputs = with pkgs;
         [
-          eigen
+          gnumake
           opencv
           boost
+          glew
+          openssl
+          eigen
           pangolin
           python3
           python3Packages.numpy
         ]
         ++ pkgs.lib.optionals isLinux [
-          # linux-specific dependencies
-          # rosPackages.melodic.ros-core
-          # rosPackages.melodic.cv-bridge
         ];
 
       # platform-specific native build inputs
       nativeBuildInputs = with pkgs;
         [
+          clang
           cmake
           pkg-config
+          librealsense
+          llvmPackages.openmp
         ]
         ++ pkgs.lib.optionals isDarwin [
-          # Darwin-specific build tools
-          # darwin.apple_sdk.frameworks.Cocoa
-          darwin.apple_sdk.frameworks.OpenGL
         ];
     };
   in {
@@ -68,24 +62,19 @@
       default = pkgs.stdenv.mkDerivation {
         pname = "orb-slam3";
         version = "0.13";
-
         src = ./.;
-
         inherit (platformConfig {inherit pkgs system;}) buildInputs nativeBuildInputs;
-
         buildPhase = ''
-          chmod +x build.sh
-          ./build.sh
+          # TODO: better scripting with just cmd runner
         '';
-
         installPhase = ''
+          # TODO: make it platform independent
           mkdir -p $out/lib
           mkdir -p $out/bin
           cp lib/libORB_SLAM3.so $out/lib/
           cp Examples/*/* $out/bin/ || true
           cp -r Vocabulary $out/
         '';
-
         meta = with pkgs.lib; {
           description = "orb-slam3: an accurate open-source library for visual, visual-inertial and multi-map slam";
           homepage = "https://github.com/thuvasooriya/orb-slam3";
@@ -101,38 +90,18 @@
       system,
     }: {
       default = pkgs.mkShell {
+        name = "os3-dev";
         buildInputs = with pkgs;
           [
             # development tools
-            gcc
-            clang
-            cmake
-            gnumake
-            boost
-            librealsense
-            llvmPackages.openmp
-            # glfw
-            glew
-            openssl
-            # libglvnd
-            # darwin.apple_sdk.frameworks.OpenGL
-            # valgrind
+            just
           ]
           ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-            # linux-specific dependencies
-            gdb
           ]
           ++ (platformConfig {inherit pkgs system;}).buildInputs;
-
         inherit (platformConfig {inherit pkgs system;}) nativeBuildInputs;
-
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-            pkgs.eigen
-            pkgs.opencv
-            pkgs.pangolin
-          ]}:$LD_LIBRARY_PATH
-        '';
+        # shellHook = ''
+        # '';
       };
     });
   };
